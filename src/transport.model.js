268c0aed0334ae764b638cd51d873389db2209c0
luidog <lui@mutesymphony.com>
2019-10-18 07:05:30 -0700

    Revises Transport
    This commit revises the transport model to add documentation and accomodate changes made to fms-api-client.

--- src/transport.model.js
+++ src/transport.model.js
@@ -3,59 +3,91 @@
 const Transport = require('winston-transport');
 const { Filemaker } = require('fms-api-client');
 
+/**
+ * @class FilemakerTransport
+ * @classdesc The class extended from winston-transport.
+ */
 class FilemakerTransport extends Transport {
-  constructor(opts) {
-    super(opts);
-    this.application = opts.application;
-    this.server = opts.server;
-    this.level = opts.level;
-    this.user = opts.user;
-    this.password = opts.password;
-    this.layout = opts.layout;
-    this.messageField = opts.messageField;
-    this.infoField = opts.infoField;
-    this.layout = opts.layout;
-    this.messageField = opts.messageField;
-    this.infoField = opts.infoField;
-  }
+  /** @constructs
+   * @param {Object} options The options to use when constructing the Transport.
+   * @param {String} options.database The FileMaker database to use for logs.
+   * @param {String} options.server The FileMaker server to use for logs..
+   * @param {String} options.user The FileMaker account to use for logs.
+   * @param {String} options.password The FileMaker account password to use.
+   * @param {String} options.layout The FileMaker layout to use when creating records.
+   * @param {String} options.messageField The FileMaker field to use for the message property.
+   * @param {String} options.infoField The FileMaker field to use for the info property.
+   */
+  constructor(options) {
+    super(options);
 
+    const {
+      database,
+      server,
+      user,
+      password,
+      layout,
+      messageField,
+      infoField
+    } = options;
+
+    this.database = database;
+    this.server = server;
+    this.user = user;
+    this.password = password;
+    this.layout = layout;
+    this.messageField = messageField;
+    this.infoField = infoField;
+  }
+  /**
+   * @method log
+   * @public
+   * @description The log method accepts an info parameter and a callback parameter. The method
+   * will call the callback method when creating the FileMaker record is complete or an error is found.
+   * @param  {object}   info     The log entry to send to FileMaker.
+   * @param  {Function} callback The function to call when logging is complete
+   * @return {Function}          The callback function to call
+   */
   log(info, callback) {
-    let { level, message, ...data } = info;
+    const { message, ...data } = info;
     if (callback === undefined) callback = () => true;
     if (!global.CLIENT) {
-      console.log('You must call connect before the FileMaker Transport will transport logs.');
+      console.log(
+        'You must call connect before the FileMaker Transport will transport logs.'
+      );
       return callback();
     }
 
-    let payload = {};
+    const payload = {};
     payload[this.messageField] = message;
     payload[this.infoField] = data;
 
-    Filemaker.findOne({ _id: this.fmId })
-      .then(client => this._createClient(client))
+    Filemaker.findOne({ _id: this._id })
+      .then(client => this.create(client))
       .then(client => client.create(this.layout, payload))
       .then(record => callback())
-      .catch(error => {
-        console.log('FileMaker Transport Error', error);
+      .catch(({ message }) => {
+        console.log('FileMaker Transport Error', message);
         callback();
       });
   }
-
-  _createClient(client) {
-    let newClient = Filemaker.create({
-      application: this.application,
-      server: this.server,
-      user: this.user,
-      password: this.password
-    });
-
-    let resolvedClient = client
+  /**
+   * [create description]
+   * @param  {Class} client An fms-api-client that has already been created.
+   * @return {Class}        Either the original client or a newly created client.
+   */
+  create(client) {
+    return client
       ? client
-      : newClient.save().then(client => {
-          this.fmId = client._id;
+      : Filemaker.create({
+          database: this.database,
+          server: this.server,
+          user: this.user,
+          password: this.password
+        }).then(client => {
+          this._id = client._id;
           return client;
         });
-    return resolvedClient;
   }
 }
 
